3cefca3d14e423a2907b0406c8eda80cf353400c
Anton Mirazchiyski <anton.mirazchiyski@gmail.com>
2025-06-16 21:57:38 +0300

    Connect db to project

--- app/database.py
+++ app/database.py
@@ -1,1 +1,16 @@
-all_bookmarks = []
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+load_dotenv()
+
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+engine = create_engine(DATABASE_URL, echo=True)
+
+SessionLocal = sessionmaker(autoflush=False, bind=engine)
+
+# all_bookmarks = []
+
